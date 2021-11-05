462521d638f5c22154bf7c03d4aac8099fa8b722
Gabriel Brun <lydstyl@gmail.com>
2021-11-05 14:53:51 +0100

    Create test data usiing UI.

---
+++ cypress/integration/ui/filter.specs.js
@@ -0,0 +1,21 @@
+describe("Filter functionalities test cases.", () => {
+    before(() => {
+        cy.visit("/")
+        ;["todo1", "todo2", "todo3", "todo4", "todo5"].forEach(todo =>
+            cy.addNewTodo(todo)
+        )
+
+        cy.get(".todo-item .todo-checkbox").first().check().should("be.checked")
+        cy.get(".todo-item .todo-checkbox").last().check().should("be.checked")
+    })
+
+    it("bla", () => {})
+
+    after(() => {
+        cy.get("body").then($el => {
+            if ($el.find(".todo-item").length) {
+                cy.get(".todo-item a").click({ multiple: true })
+            }
+        })
+    })
+})
