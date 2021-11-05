46648c636842e9d69f935b59ddc2f10e64faa179
Gabriel Brun <lydstyl@gmail.com>
2021-11-05 13:56:53 +0100

    80. Update todo status test case.

--- cypress/integration/ui/todos.specs.js
+++ cypress/integration/ui/todos.specs.js
@@ -14,4 +14,21 @@ describe("Todo UI testing", () => {
 
         cy.get(".todo-item").last().should("contain.text", todoName)
     })
+
+    it("Should be able to toggle the status of a todo correctly.", () => {
+        const todoName = "CY todo 2"
+
+        cy.get(".todo-input").type(todoName + "{enter}")
+        cy.get(".success").should("be.visible")
+
+        cy.get(".todo-item .todo-checkbox").check().should("be.checked")
+        cy.get(".todo-item .todo-checkbox").uncheck().should("not.be.checked")
+    })
+
+    afterEach(() => {
+        // cy.get(".todo-item a").each(deleteButton => {
+        //     cy.wrap(deleteButton).click()
+        // })
+        cy.get(".todo-item a").click({ multiple: true })
+    })
 })
