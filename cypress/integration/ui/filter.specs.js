0926a85c3d6761d4d0c2f296b71dabaa7676a142
Gabriel Brun <lydstyl@gmail.com>
2021-11-06 19:27:51 +0100

    91. Intercept and fixtures to mock the server.

--- cypress/integration/ui/filter.specs.js
+++ cypress/integration/ui/filter.specs.js
@@ -1,42 +1,32 @@
 describe("Filter functionalities test cases.", () => {
-    before(() => {
-        cy.addDummyTodos()
+    beforeEach(() => {
+        //cy.addDummyTodos()
+        cy.intercept(
+            {
+                method: "GET",
+                url: "http://localhost:8080/todos",
+            },
+            {
+                fixture: "todos",
+            }
+        )
 
         cy.visit("/")
-
-        // ;["todo1", "todo2", "todo3", "todo4", "todo5"].forEach(todo =>
-        //     cy.addNewTodo(todo)
-        // )
-
-        // cy.get(".todo-item .todo-checkbox").first().check().should("be.checked")
-        // cy.get(".todo-item .todo-checkbox").last().check().should("be.checked")
     })
 
     it("Should filter the completed todos correctly.", () => {
-        cy.contains("Complete").click()
-
-        cy.url().should("contain", "/complete")
-
-        cy.get(".todo-checkbox").each($todo => {
-            cy.wrap($todo).should("be.checked")
-        })
+        // cy.contains("Complete").click()
+        // cy.url().should("contain", "/complete")
+        // cy.get(".todo-checkbox").each($todo => {
+        //     cy.wrap($todo).should("be.checked")
+        // })
     })
 
     it("Should filter the active todos correctly.", () => {
-        cy.contains("Active").click()
-
-        cy.url().should("contain", "/active")
-
-        cy.get(".todo-checkbox").each($todo => {
-            cy.wrap($todo).should("not.be.checked")
-        })
-    })
-
-    after(() => {
-        cy.get("body").then($el => {
-            if ($el.find(".todo-item").length) {
-                cy.get(".todo-item a").click({ multiple: true })
-            }
-        })
+        // cy.contains("Active").click()
+        // cy.url().should("contain", "/active")
+        // cy.get(".todo-checkbox").each($todo => {
+        //     cy.wrap($todo).should("not.be.checked")
+        // })
     })
 })
